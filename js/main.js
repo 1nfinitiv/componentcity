d1446af7005cd6f318c5cf3c9c7daf38f139856a
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-08-24 16:20:51 +0500

    main

--- js/main.js
+++ js/main.js
@@ -3,14 +3,22 @@ $('.message a').click(function(){
 });
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
 let botton = document.querySelector('#click')
-
+let botton1 = document.querySelector('#click1')
 // register
 const addNewUser = async (newUser) => {
     try {
         const response = await axios.post('/registration', newUser)
         return response.data
     } catch (err) {
-        console.error(err.toJSON())
+        console.error(err.response.data.message)
+    }
+}
+const LogIn = async (data)=>{
+    try{
+        const response = await axios.post('/login', data)
+        return response.data
+    }catch (err){
+        console.error(err.response.data.message)
     }
 }
 botton.addEventListener('click', function(){
@@ -19,4 +27,10 @@ botton.addEventListener('click', function(){
     let name = document.querySelector('#name').value
     let email = document.querySelector('#email').value
     addNewUser({User_name:name, User_password:password, Email_adress:email})
+})
+botton1.addEventListener('click', function (){
+    console.log('work')
+    let password = document.querySelector('#password1').value
+    let login = document.querySelector('#username').value
+    LogIn({User_name: login, User_password: password})
 })
